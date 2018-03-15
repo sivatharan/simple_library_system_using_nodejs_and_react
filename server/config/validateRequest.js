3765869c930cff24df47393614a37c372ece013b
sivatharan <sivatharan.uop@gmail.com>
2018-03-15 10:31:26 +0530

    authenticated added

--- server/config/validateRequest.js
+++ server/config/validateRequest.js
@@ -2,6 +2,8 @@ var jwt = require('jsonwebtoken');
 
 
 module.exports = function(req, res, next) {
+	//auth function here
+	// if(err) return res.status(400).json({status: false, message: "failed", result:result}); 
     next(); // 
 };
 
