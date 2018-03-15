48da4f0b4165f31e4ff8b084c4d0d6986e5edc93
sivatharan <sivatharan.uop@gmail.com>
2018-03-15 10:35:51 +0530

    authentication added

--- server/config/validateRequest.js
+++ server/config/validateRequest.js
@@ -3,7 +3,7 @@ var jwt = require('jsonwebtoken');
 
 module.exports = function(req, res, next) {
 	//auth function here
-	// if(err) return res.status(400).json({status: false, message: "failed", result:result}); 
+	// if(err) return res.status(400).json({status: false, message: "failed", result:'somthing went wrong'}); 
     next(); // 
 };
 
